a3aedb0dff87ea07d4792ed311decdd74835efc9
GoatlyGod <bambijames297@yahoo.com>
2020-01-31 18:42:21 +0000

    init commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,42 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  const suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  const cardNumber = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+
+  const suitElementNum = Math.floor(Math.random() * suit.length);
+  const cardNumberElementNum = Math.floor(Math.random() * cardNumber.length);
+  const suitElement = suit[suitElementNum];
+  const cardNumberElement = cardNumber[cardNumberElementNum];
+
+  this.document.getElementById("cardContent").innerHTML = cardNumberElement;
+  this.document.getElementById("theCard").add = suitElement;
 };
+
+function getSuitClass(suit) {
+  switch (suit) {
+    case "Diamonds":
+      return "suit-diamonds";
+    case "Spades":
+      return "suit-spades";
+    case "Hearts":
+      return "suit-hearts";
+    case "Clubs":
+      return "suit-clubs";
+  }
+}
